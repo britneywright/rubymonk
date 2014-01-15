d21a3266e90442cf048de953e1d8bcdcbaf924bc
Britney Wright <blw06g@gmail.com>
2014-01-14 19:16:05 -0500

    added colors problem

--- kaprekar.rb
+++ kaprekar.rb
@@ -1,4 +1,13 @@
+#updating this one
+
 def kaprekar?(k)
   n = (k ** 2).to_s.split(/\s*/)
   (n.first((n.size)/2).join.to_i) + (n.last((n.size + 1)/2).join.to_i) == k
 end
+
+def halves(n)
+	[
+		n[0...(n.size / 2)],
+		n[(n.size/2)..-1]
+	]
+end	
